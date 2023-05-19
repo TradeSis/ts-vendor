95074c22ca62f7c1899e3f6ea7f8e8ef14a7b5e9
Helio Tradesis <helio@tradesis.com.br>
2023-05-19 16:24:19 -0300

    helio ajustes site
    novo vendor.php

---
+++ vendor.php
@@ -0,0 +1,42 @@
+<?php
+include_once __DIR__ . "/../config.php";
+?>
+
+<head>
+        <title>TS/painel</title>
+
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+        <script src="<?php echo URLROOT ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/animacoes/bodymovin.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/jquery/jquery.min.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/jquery/jquery.mask.min.js" type="text/javascript"></script>
+        <script src="<?php echo URLROOT ?>/vendor/jquery/tabletojson.min.js" type="text/javascript"></script>
+        <script src="<?php echo URLROOT ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+        <script src="<?php echo URLROOT ?>/vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="<?php echo URLROOT ?>/vendor/bootstrap/bootbox/bootbox.min.js" type="text/javascript"></script>
+        <link href="<?php echo URLROOT ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <link rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+        <link href="<?php echo URLROOT ?>/painel/css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="<?php echo URLROOT ?>/painel/css/padrao.css" rel="stylesheet" type="text/css">
+        <link href="<?php echo URLROOT ?>/painel/css/menu.css" rel="stylesheet" type="text/css">
+
+        <script src="<?php echo URLROOT ?>/painel/js/input.js"></script>
+
+
+      
+        <link href="<?php echo URLROOT ?>/vendor/aos/aos.css" rel="stylesheet"> <!-- Transição dos elementos -->
+        <script src="<?php echo URLROOT ?>/vendor/aos/aos.js"></script>
+        <link href="<?php echo URLROOT ?>/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+        <script src="<?php echo URLROOT ?>/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/glightbox/js/glightbox.min.js"></script>
+    
+
+</head>
