c8520cc4b8e3df3cfbdbd0c1bcdef758a7acde8b
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-05 17:12:19 -0300

    adicionado arquivos do footer_js que estavam em sistema

--- footer_js.php
+++ footer_js.php
@@ -9,6 +9,11 @@
         <script src="<?php echo URLROOT ?>/vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
         <script src="<?php echo URLROOT ?>/vendor/bootstrap/bootbox/bootbox.min.js" type="text/javascript"></script>
 
+        <!-- bacote js bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <!-- jquery -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
         <!-- input dos formularios -->
         <script src="<?php echo URLROOT ?>/sistema/js/input.js"></script>
 
