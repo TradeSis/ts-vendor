782dc43c70af20cc68c98c102608e30153c2e10f
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-05 17:07:29 -0300

    separando arquivos do vendor

---
+++ footer_js.php
@@ -0,0 +1,27 @@
+
+        <!---Relacionados ao bootstrap -->
+        <script src="<?php echo URLROOT ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/animacoes/bodymovin.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/jquery/jquery.min.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/jquery/jquery.mask.min.js" type="text/javascript"></script>
+        <script src="<?php echo URLROOT ?>/vendor/jquery/tabletojson.min.js" type="text/javascript"></script>
+        <script src="<?php echo URLROOT ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="<?php echo URLROOT ?>/vendor/bootstrap/bootbox/bootbox.min.js" type="text/javascript"></script>
+
+        <!-- input dos formularios -->
+        <script src="<?php echo URLROOT ?>/sistema/js/input.js"></script>
+
+        <!-- Calendario -->
+        <script type="text/javascript" src="<?php echo URLROOT ?>/vendor/fullcalendar/moment.min.js"></script>
+        <script type="text/javascript" src="<?php echo URLROOT ?>/vendor/fullcalendar/fullcalendar.min.js"></script>
+        <script type="text/javascript" src="<?php echo URLROOT ?>/vendor/fullcalendar/pt-br.min.js"></script>
+
+        <!-- Transição dos elementos de paginas.php -->
+        <script src="<?php echo URLROOT ?>/vendor/aos/aos.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="<?php echo URLROOT ?>/vendor/glightbox/js/glightbox.min.js"></script>
+
+        <!-- QUILL editor -->
+        <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+
