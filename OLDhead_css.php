09c89f22f48507bc1eb9827fa284b5e2ccbf8599
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-20 15:45:54 -0300

    removido arquivos duplicados

--- OLDhead_css.php
+++ OLDhead_css.php
@@ -6,7 +6,7 @@ include_once __DIR__ . "/../config.php";
         <!-- Icone da Pagina -->
         <link rel="icon" href="<?php echo URLROOT ?>/img/meucontrole_icon.png" type="image/png">
 
-        <!---Adicionado cdn dpo Bootstrap 5.0.2 -->
+        <!---Adicionado cdn dpo Bootstrap 5.0.2, Mais ainda não foi removida a versao do bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
         <!---Relacionados ao bootstrap -->
@@ -37,3 +37,4 @@ include_once __DIR__ . "/../config.php";
         <!-- QUILL editor -->
         <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
  
+
