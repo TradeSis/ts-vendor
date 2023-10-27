20951d3d1070502b2fc1a8a5157448b7de10b69b
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-27 16:08:25 -0300

    removido todo bootstrap 4 do vendor

--- head_css.php
+++ head_css.php
@@ -9,10 +9,8 @@ include_once __DIR__ . "/../config.php";
         <!---Adicionado cdn dpo Bootstrap 5.0.2 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <!---Relacionados ao bootstrap -->
-        <link href="<?php echo URLROOT ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <!---Bootstrap Icon -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
         <!-- CSS -->
         <link href="<?php echo URLROOT ?>/sistema/css/bootstrap.css" rel="stylesheet" type="text/css">
@@ -27,6 +25,7 @@ include_once __DIR__ . "/../config.php";
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
         <!-- Fonte Inter -->
        
         <!-- QUILL editor -->
