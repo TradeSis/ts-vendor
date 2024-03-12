04a4025e23eb7ef3e469dfc015da297a28204dd4
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2024-03-12 16:27:45 -0300

    update

--- footer_js.php
+++ footer_js.php
@@ -1,6 +1,4 @@
 
-        <!-- pacote js bootstrap -->
-        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
         <!-- Bootstrap Atualizado -->
         <script src="<?php echo URLROOT ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
         <!-- jquery -->
